b128dea6376f4eda6342d279220f121d21ebf788
GAILLET Pierre-Paul <PSR10528@students.ephec.be>
2019-02-18 16:24:39 +0100

    creation feedmecontext

---
+++ FeedMe/Models/FeedMeContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FeedMe.Models
+{
+    public class FeedMeContext : DbContext
+    {
+
+        public DbSet<CuisineTypeObj> CuisineTypes { get; set; }
+
+        public DbSet<DishesObj> Dishes { get; set; }
+
+        public DbSet<ImagesObj> Images { get; set; }
+
+        public DbSet<MenusObj> Menus { get; set; }
+
+        public DbSet<RestaurantsObj> Restaurants { get; set; }
+
+    }
+}
