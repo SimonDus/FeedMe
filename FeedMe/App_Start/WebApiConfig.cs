480f5a2603ef09ab18382e893b91117e299d09be
gailletpp <gailletpp@gmail.com>
2019-02-19 10:06:45 +0100

    json default with lower case

--- FeedMe/App_Start/WebApiConfig.cs
+++ FeedMe/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -11,6 +12,10 @@ namespace FeedMe
         {
             // Web API configuration and services
 
+            
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new LowercaseContractResolver();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +24,14 @@ namespace FeedMe
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+    }
+
+    public class LowercaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return propertyName.ToLower();
         }
     }
 }
